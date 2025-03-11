6fddaf7a4dcff94a561892fffeebf34f7cf4e645
Zdravko Bozhkov <zdravko6t5@gmail.com>
2025-03-11 15:12:25 -0400

    fist commit

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,142 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Next.JS</li>
+        <li>JavaScript</li>
+        <li>Node.JS</li>
+        <li>Google Cloud</li>
+        <li>Generative AI</li>
+        <li>Java</li>
+        <li>Python</li>
+        <li>MySQL</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Tools",
+    id: "tools",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Visual Studio Code</li>
+        <li>Postman</li>
+        <li>IntelliJ IDEA</li>
+        <li>Figma</li>
+        <li>LM Studio</li>
+        <li>Cursor</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <h5>B.S. Computer Science, Kennesaw State University</h5>
+        <br></br>
+        <li>
+          <b>Publication</b> - Intelligent-Checkout System Research Paper.
+        </li>
+        <li>
+          <b>Capstone Project</b> - Led a 2-member team of highly motivated
+          software engineers in establishing autonomous protection for a
+          defensive autonomous sentry gun by utilizing the latest machine
+          learning, generative AI libraries and IoT devices.
+        </li>
+        <li>
+          <b>Organizations</b> - Member of Association for Computing Machinery,
+          IEEE Computer Society.
+        </li>
+        <li>
+          <b>Volunteer</b> - 2019 CCSE Hackathon Student Volunteer. 2018
+          Hackathon Participator at Clemson University.
+        </li>
+        <li>
+          <b>Related Coursework</b> - Data Structures, Algorithm Analysis,
+          Database Systems, Operating Systems, Machine Vision, AI, Internet
+          Programming.
+        </li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>
+          <b>DeepLearning.AI</b> - Introductory to Generative AI for Software
+          Development
+        </li>
+        <li>
+          <b>Google Professional Cloud Developer</b> - Scaling and Applying
+          Automation to an elastic Google Cloud Infrastructure Console Project
+        </li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/about.png" width={500} height={500} alt="about image" />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am a full-stack developer with experience creating intuitive
+            applications utilizing the latest frameworks and languages including
+            Next.JS, Java, and Python. I also have knowledge in Google Cloud,
+            Generative AI technologies and setting up development environments
+            with Jenkins for a collaborative session and growth.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/app/projectsdata.js
@@ -0,0 +1,102 @@
+export const projectsData = [
+  {
+    id: 1,
+    title: "AI Trading Bot",
+    description:
+      "Stock market trading prediction bot built with Python that predicts when to buy and sell a stock based on market evaluations, past trading data and grows smarter over time.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/angularportfolio-71eaa.appspot.com/o/aitradingbot.png?alt=media&token=30fcdba6-c2f6-488c-bf20-9563376ca3d2",
+    tag: ["All", "App"],
+    gitUrl: "https://github.com/jamesb97/AlgorithmicTrading",
+    previewUrl:
+      "https://colab.research.google.com/drive/1DqdRsjZY9axy0Uqf7mLnstTyzR5T2VFH#scrollTo=cdHONQSWC5Q4",
+  },
+  {
+    id: 2,
+    title: "Autonomous Sentry Gun",
+    description:
+      "Motion detection and object recognition sentry gun capable of detecting objects in real time using OpenCV, Python, C++, and Raspberry Pi.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/angularportfolio-71eaa.appspot.com/o/0.jpg?alt=media&token=5ebe5cd1-cb09-4b43-8ca5-dd2c366119a1",
+    tag: ["All", "App"],
+    gitUrl: "https://github.com/jamesb97/AutonomousSentryGun",
+    previewUrl: "/",
+  },
+  {
+    id: 3,
+    title: "React Recipe Book",
+    description:
+      "Online recipe tool, curated with dozen of useful recipes for anyone looking to make their next meal. Created using React and Vite and utilizes the Edamame API to communicate with the recipes.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/angularportfolio-71eaa.appspot.com/o/6.png?alt=media&token=9bb02ad3-cc48-4e84-87d8-effe84986d2d",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "https://infallible-hamilton-a8e2a2.netlify.app/",
+  },
+  {
+    id: 4,
+    title: "Netflix Clone",
+    description:
+      "A redesigned version of Netflix created with Firebase and React.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/angularportfolio-71eaa.appspot.com/o/5.png?alt=media&token=93411691-b535-44f2-bd7a-2e5ada0f4e52",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "https://netflix-clone-3b31f.firebaseapp.com/",
+  },
+  {
+    id: 5,
+    title: "Shortest Path Algorithm",
+    description:
+      "Website that uses React for the front-end and Node.JS for the backend to visualize the first node and determine the shortest path needed to take in order to get to the second node using Dijkstra's Algorithm.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/angularportfolio-71eaa.appspot.com/o/7.png?alt=media&token=9d2be112-d0c0-4548-84ff-17e4ae6bc54b",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "https://determined-lamarr-304345.netlify.app/",
+  },
+  {
+    id: 6,
+    title: "News Scraper",
+    description:
+      "An intuitive Node.JS application that scrapes recent headlines and data from a website and then writes them to a CSV file.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/angularportfolio-71eaa.appspot.com/o/8.png?alt=media&token=8eda1abb-d1cb-4c25-ac62-22175863d024",
+    tag: ["All", "App"],
+    gitUrl: "https://github.com/jamesb97/NewsScrape",
+    previewUrl: "/",
+  },
+  {
+    id: 7,
+    title: "Intelligent Checkout System",
+    description:
+      "A smart checkout system geared towards a faster and seamless checkout experience at a local convenient stores by utilizing the latest machine learning libraries including OpenCV, PyTorch, NVIDIA CUDA installed on the NVIDIA Jetson Nano.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/angularportfolio-71eaa.appspot.com/o/1.jpg?alt=media&token=e0591058-c8f7-42dd-b712-58ae95534df3",
+    tag: ["All", "App"],
+    gitUrl: "https://github.com/jiaming-cs/Intelligent-Checkout-System",
+    previewUrl: "https://bit.ly/ChecSys",
+  },
+  {
+    id: 8,
+    title: "Spotify Clone",
+    description:
+      "A remastered version of Spotify for the web created using React, React Redux, React Hooks, and Spotify API.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/angularportfolio-71eaa.appspot.com/o/2.png?alt=media&token=c35c1f9f-46d1-426e-b2e1-7d81c512cb72",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "https://updateprofile-valtqe.web.app/",
+  },
+  {
+    id: 9,
+    title: "PDF Scanner",
+    description:
+      "Portable Android PDF Scanner made with React that uses OpenCV Machine Vision library to capture the document and adjustment.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/angularportfolio-71eaa.appspot.com/o/10.png?alt=media&token=64af005d-d79b-4971-96fa-09c708f2526e",
+    tag: ["All", "Mobile"],
+    gitUrl: "https://github.com/jamesb97/PDFScanner",
+    previewUrl: "https://vimeo.com/605873299",
+  },
+];
