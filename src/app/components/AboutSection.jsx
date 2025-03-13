85cdd3b5f414b6aa977037c6793ab86fafd8b190
Zdravko Bozhkov <zdravko6t5@gmail.com>
2025-03-13 01:22:00 -0400

    minor updates

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -97,7 +97,12 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/about.png" width={500} height={500} alt="about image" />
+        <Image
+          src="https://firebasestorage.googleapis.com/v0/b/angularportfolio-71eaa.appspot.com/o/about.png?alt=media&token=b618a30e-e999-4671-aa83-395d4088902a"
+          width={500}
+          height={500}
+          alt="about image"
+        />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
