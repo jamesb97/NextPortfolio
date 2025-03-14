395b940c6593d2eef0f156774c48775326aa6c01
Zdravko Bozhkov <zdravko6t5@gmail.com>
2025-03-14 11:27:14 -0400

    minor updates

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import Image from "next/image";
+//import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 import Link from "next/link";
@@ -66,12 +66,12 @@ const HeroSection = () => {
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-            <Image
-              src="https://firebasestorage.googleapis.com/v0/b/angularportfolio-71eaa.appspot.com/o/hero.png?alt=media&token=b0aabd99-c390-4c86-9cfa-48d5ff310f28"
-              alt="hero image"
+            <img
+              src="/images/hero.png"
+              alt="hero-image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full"
-              width={300}
-              height={300}
+              width="300"
+              height="300"
             />
           </div>
         </motion.div>
