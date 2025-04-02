5e3840ead35579c63696aea0da6e036104f99489
Zdravko Bozhkov <zdravko6t5@gmail.com>
2025-04-02 19:25:22 -0400

    added design page

---
+++ src/app/designs/designs.js
@@ -0,0 +1,44 @@
+export const DESIGNS = [
+  {
+    id: 1,
+    title: "Vogue Cover",
+    description: "Serena Prestwood representing Calvin Klein",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/fir-stream-19493.appspot.com/o/VogueCover.png?alt=media&token=8df81a15-a5b7-4606-8d8f-8f81cbc8df78",
+    tag: ["All"],
+  },
+  {
+    id: 2,
+    title: "Party Flyer",
+    description: "A summer splash pool party is happening near Santa Monica",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/fir-stream-19493.appspot.com/o/PartyFlyer.png?alt=media&token=ee6bd0e8-2452-4c0e-8168-52ef8b2a6fe8",
+    tag: ["All"],
+  },
+  {
+    id: 3,
+    title: "Shattered Dimensions",
+    description:
+      "Broken promises and shattered dreams of individual pieces are scattered on the floor",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/fir-stream-19493.appspot.com/o/bozhkov_A2.png?alt=media&token=fda039b6-a242-4f41-bda2-091b547d4923",
+    tag: ["All"],
+  },
+  {
+    id: 4,
+    title: "A - Z",
+    description: "Realtime objects represented in the shape of the alphabet",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/fir-stream-19493.appspot.com/o/bozhkov_A10.jpg?alt=media&token=98254953-d5c4-4e87-be48-200bad765b4f",
+    tag: ["All"],
+  },
+  {
+    id: 5,
+    title: "Dangerous",
+    description:
+      "Dangerous dictatorship is happening in Cuba as Che Guevara takes the spot",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/fir-stream-19493.appspot.com/o/Bozhkov-15.png?alt=media&token=2bf127a4-2b9f-4f57-91df-02eb6eef6ce6",
+    tag: ["All"],
+  },
+];
---
+++ src/app/designs/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h6>Design Page</h6>
+    </div>
+  );
+};
+
+export default page;
