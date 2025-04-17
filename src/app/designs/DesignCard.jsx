f397bc71794cb0204c748c54293b688580b6350e
Zdravko Bozhkov <zdravko6t5@gmail.com>
2025-04-17 02:14:59 -0400

    updated several design details

--- src/app/designs/DesignCard.jsx
+++ src/app/designs/DesignCard.jsx
@@ -1,37 +1,32 @@
 import Image from "next/image";
-import { motion } from "framer-motion";
+//import { motion } from "framer-motion";
 
 const DesignCard = ({ title, description, imageUrl, link }) => {
   return (
-    <motion.div
-      className="bg-white rounded-2xl shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300"
-      initial={{ opacity: 0, y: 40 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
-      //   whileHover={{ scale: 1.03 }}
-    >
+    <div className="bg-white rounded-xl shadow-md overflow-hidden relative group h-52 md:h-72">
       <Image
         src={imageUrl}
         alt={title}
-        width={600}
-        height={400}
-        className="object-cover w-full h-48"
+        width={200}
+        height={200}
+        className="object-cover w-full h-40"
       />
-      <div className="p-4">
-        <h3 className="text-xl font-semibold mb-2">{title}</h3>
-        <p className="text-gray-600">{description}</p>
-        {link && (
-          <a
-            href={link}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-block mt-4 text-blue-600 hover:underline"
-          >
-            View Design →
-          </a>
-        )}
-      </div>
-    </motion.div>
+      <div className="p-3"></div>
+      <h3 className="text-lg text-black font-semibold mb-1 mt-2 px-2">
+        {title}
+      </h3>
+      <p className="text-gray-600 text-sm mb-2 px-2">{description}</p>
+      {link && (
+        <a
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-block mt-3 text-blue-600 hover:underline text-sm mb-2 px-2"
+        >
+          View Design →
+        </a>
+      )}
+    </div>
   );
 };
 
--- src/app/designs/page.jsx
+++ src/app/designs/page.jsx
@@ -1,9 +1,45 @@
 import React from "react";
+import DesignCard from "./DesignCard";
+import Navbar from "../components/Navbar";
+import Image from "next/image";
 
 const page = () => {
   return (
-    <div>
-      <h6>Design Page</h6>
+    <div className="flex min-h-screen flex-col bg-[#121212]">
+      <Navbar />
+      <div className="container mt-24 mx-auto px-12 py-4">
+        <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+          My Designs
+        </h2>
+        <br />
+        <p className="text-center mb-4">
+          A list of some of my designs created with Adobe Suite.
+        </p>
+        <br />
+        <main className="h-52 md:h-72 rounded-t-xl relative group px-4 py-12">
+          <div className="flex overflow-x-auto space-x-6 pb-4 scrollbar-hide">
+            {/* <main className="h-52 md:h-72 rounded-t-xl relative-group" style={{ background: `url(${imgUrl})`}}> */}
+            <DesignCard
+              title="Coffee"
+              description="Warm energetic colors"
+              imageUrl="https://firebasestorage.googleapis.com/v0/b/angulardesigns-aaa7c.appspot.com/o/coffee1.png?alt=media&token=69013e9a-0cb0-4ba0-bd07-93807512e2d0"
+              link="https://firebasestorage.googleapis.com/v0/b/angulardesigns-aaa7c.appspot.com/o/coffee1.png?alt=media&token=69013e9a-0cb0-4ba0-bd07-93807512e2d0"
+            />
+            <DesignCard
+              title="Hamburger"
+              description="Various topping mixtures"
+              imageUrl="https://firebasestorage.googleapis.com/v0/b/angulardesigns-aaa7c.appspot.com/o/hamburger.png?alt=media&token=39bb75c9-b062-4d34-b8f6-1453495f4391"
+              link="https://firebasestorage.googleapis.com/v0/b/angulardesigns-aaa7c.appspot.com/o/hamburger.png?alt=media&token=39bb75c9-b062-4d34-b8f6-1453495f4391"
+            />
+            <DesignCard
+              title="Launch"
+              description="Warm saturated color palette"
+              imageUrl="https://firebasestorage.googleapis.com/v0/b/angulardesigns-aaa7c.appspot.com/o/rocketship.png?alt=media&token=d7e20a0c-69f4-48ea-a446-71dacd22e7e3"
+              link="https://firebasestorage.googleapis.com/v0/b/angulardesigns-aaa7c.appspot.com/o/rocketship.png?alt=media&token=d7e20a0c-69f4-48ea-a446-71dacd22e7e3"
+            />
+          </div>
+        </main>
+      </div>
     </div>
   );
 };
