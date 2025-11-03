fce9f4517621143e3cab0f6731acab0395f3f78d
Zdravko Bozhkov <zdravko6t5@gmail.com>
2025-11-03 12:08:04 -0500

    updated footer details

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 
 const Footer = () => {
@@ -5,7 +6,9 @@ const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
-        <span>Zdravko</span>
+        <span onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}>
+          Zdravko
+        </span>
         <p className="text-slate-600">
           &#169; Zdravko Bozhkov {currentYear}. All rights reserved.
         </p>
