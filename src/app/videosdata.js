c980e9462fb24492d1506925ac926a135f95ab01
Zdravko Bozhkov <zdravko6t5@gmail.com>
2026-07-17 14:08:30 -0400

    fixed incorrect images

--- src/app/videosdata.js
+++ src/app/videosdata.js
@@ -2,7 +2,7 @@ export const videosData = [
   {
     id: 1,
     image:
-      "https://firebasestorage.googleapis.com/v0/b/angularportfolio-71eaa.appspot.com/o/autonomous-technology.webp?alt=media&token=d8f1a5dd-74d7-49a8-9b5d-8a6d369424bf",
+      "https://res.cloudinary.com/dppg3f01m/image/upload/v1784235815/autonomousvehicles_fqt8zi.png",
     title: "Autonomous Vehicles and AI",
     description:
       "A deep dive into how autonomous vehicles use artificial intelligence to navigate and make decisions on the road.",
@@ -11,7 +11,7 @@ export const videosData = [
   {
     id: 2,
     image:
-      "https://firebasestorage.googleapis.com/v0/b/angularportfolio-71eaa.appspot.com/o/intelligent-checkout-system.webp?alt=media&token=7f6586ce-9dc2-4e7c-a4ce-22f3d51e5b57",
+      "https://storage.googleapis.com/zdravko6t5.site/intelligent-checkout-system.webp",
     title: "Intelligent Checkout System",
     description:
       "An overview of a smart checkout system that utilizes machine learning and computer vision to streamline the checkout process in retail stores.",
@@ -20,7 +20,7 @@ export const videosData = [
   {
     id: 3,
     image:
-      "https://firebasestorage.googleapis.com/v0/b/angularportfolio-71eaa.appspot.com/o/architect-toolkit.webp?alt=media&token=4834c4d2-9411-4289-bb05-063770492a52",
+      "https://storage.googleapis.com/zdravko6t5.site/architect-toolkit.webp",
     title: "Architect's Toolkit",
     description:
       "A look into a comprehensive software suite designed to assist architects in designing, modeling, and visualizing their projects.",
