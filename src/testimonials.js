b8a49da1724dabae8da1f7f3b29e6d0bde6084f2
Zdravko Bozhkov <zdravko6t5@gmail.com>
2026-07-21 13:37:32 -0400

    added sample description

--- src/testimonials.js
+++ src/testimonials.js
@@ -1,13 +1,15 @@
 export const testimonials = [
   {
     name: "James Barnett",
-    description: "This is a test testimonial",
+    description:
+      "Zdravko is a great developer and a great person. He is always willing to help in any project that involves critial thinking and is a great team player.",
     image:
       "https://res.cloudinary.com/dppg3f01m/image/upload/v1784654936/james_nmgplg.jpg",
   },
   {
     name: "Jack Shalev",
-    description: "This is a test testimonial",
+    description:
+      "Zdravko is a great developer and a great person. He is always willing to help in any project that involves critial thinking and is a great team player.",
     image:
       "https://res.cloudinary.com/dppg3f01m/image/upload/v1784654831/jack_wt1vk4.jpg",
   },
