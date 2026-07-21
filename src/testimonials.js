03505da4d1cdc78e1b896ce84c7f202d19cb4830
Zdravko Bozhkov <zdravko6t5@gmail.com>
2026-07-21 13:32:38 -0400

    inserted sample Testimonials data

---
+++ src/testimonials.js
@@ -0,0 +1,14 @@
+export const testimonials = [
+  {
+    name: "James Barnett",
+    description: "This is a test testimonial",
+    image:
+      "https://res.cloudinary.com/dppg3f01m/image/upload/v1784654936/james_nmgplg.jpg",
+  },
+  {
+    name: "Jack Shalev",
+    description: "This is a test testimonial",
+    image:
+      "https://res.cloudinary.com/dppg3f01m/image/upload/v1784654831/jack_wt1vk4.jpg",
+  },
+];
